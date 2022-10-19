bbb94fd207eb0d29cdcad407ab0bfdeb60f5805a
erikmya <erik.maya@librerialeo.com.mx>
2022-10-19 12:44:47 -0500

    Clase 6 Nodemon y Pm2

--- entorno.js
+++ entorno.js
@@ -9,3 +9,13 @@ console.log('Mi web es ' + web);
 //$env:NOMBRE="Erik"
 //$env:WEB="erik.com"
 //node entorno.js
+
+console.log('Otra cosa...');
+
+//Comandos nodemon
+//npm i -g nodemon
+//nodemon .\entorno.js - para ejecutar y hacer cambios en vivo un archivo
+
+//Comandos pm2
+//npm install -g pm2
+//pm2 start .\entorno.js
--- monohilo.js
+++ monohilo.js
@@ -6,10 +6,10 @@ setInterval(function() {
     console.log(i);
     i++;
 
-    if (i === 5) {
-        console.log('forzamos error');
-        var a = 3 + z;
-    }
+    // if (i === 5) {
+    //     console.log('forzamos error');
+    //     var a = 3 + z;
+    // }
 }, 1000);
 
-console.log('Segunda instruccion');
+console.log('Ultima instruccion');
